ed0f7c2e0c05c719ac1831e912a43ea1bac0d787
Junio Xavier <jrmoreira18@hotmail.com>
2022-09-07 10:24:44 -0300

    create: room relationship with hotel

--- rooms/models.py
+++ rooms/models.py
@@ -11,3 +11,5 @@ class Room(models.Model):
     description = models.TextField()
     rent_price = models.DecimalField(max_digits=8, decimal_places=2)
     floor = models.CharField(max_length=100)
+
+    hotel = models.ForeignKey("hotels.Hotel", on_delete=models.CASCADE, related_name="rooms")
