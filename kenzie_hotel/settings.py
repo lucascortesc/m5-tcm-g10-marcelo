6d1274767ad01fc31e7a3dee9673bfffc72ac6dd
lucascortesc <ccordeiro.lucas@gmail.com>
2022-09-09 19:26:54 -0300

    fix: heroku config

--- kenzie_hotel/settings.py
+++ kenzie_hotel/settings.py
@@ -157,3 +157,5 @@ if DATABASE_URL:
     db_from_env = dj_database_url.config(
         default=DATABASE_URL, conn_max_age=500, ssl_require=True)
     DATABASES['default'].update(db_from_env)
+
+    DEBUG = True
