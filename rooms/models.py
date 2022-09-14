ea3ebf711b47988614201886d319e07d01bee300
lucascortesc <ccordeiro.lucas@gmail.com>
2022-09-14 19:16:02 -0300

    feat: checkin / checkout

--- rooms/models.py
+++ rooms/models.py
@@ -1,16 +1,19 @@
-from calendar import c
-from django.db import models
 import uuid
+
+from django.core.validators import MinValueValidator
+from django.db import models
 
 
 class Room(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     number = models.CharField(max_length=15)
-    number_of_beds = models.IntegerField()
+    number_of_beds = models.PositiveIntegerField()
     is_vacant = models.BooleanField(default=True)
-    capacity = models.IntegerField()
+    capacity = models.PositiveIntegerField()
     description = models.TextField()
-    rent_price = models.DecimalField(max_digits=8, decimal_places=2)
+    rent_price = models.DecimalField(
+        max_digits=8, decimal_places=2, validators=[MinValueValidator(0)]
+        )
     floor = models.CharField(max_length=100)
 
     hotel = models.ForeignKey(
--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,7 @@
-import operator
-from functools import reduce
-
-from django.db.models import Q
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from django_filters.rest_framework import DjangoFilterBackend
 
 from rooms.models import Room, RoomAmenity
 from rooms.serializers import RoomSerializer
@@ -29,23 +25,8 @@ class RoomView(generics.ListCreateAPIView):
         serializer.save(hotel=self.request.user)
 
     def get_queryset(self):
-        #return self.queryset.filter(hotel=self.request.user)
+
         params_amenities = self.request.GET.getlist('amenities')
-        # rooms = Room.objects.all()
-
-        # filtred_rooms = []
-
-        # for room in rooms:
-        #     length = len(params_amenities)
-
-        #     for amenity in room.amenities.all():
-        #         if amenity.name in params_amenities:
-        #             length -= 1 
-
-        #     if length == 0:
-        #         filtred_rooms.append(room)
-                    
-        # return filtred_rooms
 
         queryset = Room.objects.all()
         for search_term in params_amenities:
@@ -53,29 +34,9 @@ class RoomView(generics.ListCreateAPIView):
         
         return queryset
 
-        
-
 
 class RoomDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
     lookup_url_kwarg = "room_id"
-    
-"""
-class RoomFilterView(generics.ListAPIView):
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
-    queryset = Room.objects.all()
-    serializer_class = RoomSerializer
-
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = [
-        "number_of_beds",
-        "capacity",
-        "rent_price",
-        "amenities",
-    ]
-
-"""
