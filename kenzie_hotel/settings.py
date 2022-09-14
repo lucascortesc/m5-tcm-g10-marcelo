8397c8d44e83569ae2b40d374da4b94fdd278088
Junio Xavier <jrmoreira18@hotmail.com>
2022-09-14 17:53:22 -0300

    create: api doc

--- kenzie_hotel/settings.py
+++ kenzie_hotel/settings.py
@@ -159,3 +159,15 @@ if DATABASE_URL:
 
     DATABASES["default"].update(db)
     DEBUG = True
+
+REST_FRAMEWORK = {
+   
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Kenzie_Hotel_API',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    
+}
--- kenzie_hotel/urls.py
+++ kenzie_hotel/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework.authtoken.views import obtain_auth_token
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -25,5 +26,7 @@ urlpatterns = [
     path("api/", include("guests.urls")),
     path("api/", include("reservations.urls")),
     path("api/login/", obtain_auth_token),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
 
 ]
